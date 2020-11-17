ea1df001cfe60cb0f0a2c956902ba1d0456f55d5
mksmdvdff <mksmdavydoff@gmail.com>
2020-11-17 07:48:51 +0300

    add ft_strcpy

---
+++ ft_strcpy.s
@@ -0,0 +1,20 @@
+global _ft_strcpy
+
+section .text
+_ft_strcpy:
+				push rdi
+				push rdx
+				xor rax, rax
+loop:
+				cmp byte[rsi + rax], 0
+				je	return
+				mov dl, byte[rsi + rax]
+				mov byte[rdi + rax], dl
+				inc rax
+				jmp loop
+return:
+				mov byte[rdi + rax], 0
+				mov rax, rdi
+				pop rdx
+				pop rdi
+				ret
