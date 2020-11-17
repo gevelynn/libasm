0a2368c718f2551890b54f03caaa8b147c644e39
mksmdvdff <mksmdavydoff@gmail.com>
2020-11-17 08:27:14 +0300

    add ft_write

---
+++ ft_write.s
@@ -0,0 +1,17 @@
+global _ft_read
+extern ___error
+section .text
+
+_ft_read:
+				mov rax, 0x2000004
+				syscall
+				jc return_error
+				ret
+return_error:
+				push r8
+				mov r8, rax
+				call ___error
+				mov [rax], r8
+				mov rax, -1
+				pop r8
+				ret
